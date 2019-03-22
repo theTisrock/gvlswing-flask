7bb813cb856978ef53762c3fdf04523688aaeed3
theTisrock <torok.chris@gmail.com>
2019-03-22 01:50:41 -0400

    "Added error handler to app."

--- gvlswing_app/errors.py
+++ gvlswing_app/errors.py
@@ -4,11 +4,11 @@ from gvlswing_app import app
 
 @app.errorhandler(404)
 def http_not_found_error(error):
-    return "What you requested doesn't exist."
+    return "What you requested doesn't exist.", 404
 
 
 @app.errorhandler(500)
 def http_application_error(error):
-    return "It's not you ... it's me"
+    return "It's not you ... it's me", 500
 
 # end errors
