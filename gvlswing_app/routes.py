befd6d87c2ab2e5a7835aff7ce9a19d02dcd64e5
Christopher Torok <torok.chris@gmail.com>
2019-04-01 04:00:59 -0400

    Added Actions & URLs in urls.py

--- gvlswing_app/routes.py
+++ gvlswing_app/routes.py
@@ -6,19 +6,20 @@
 # ???
 
 from gvlswing_app import app, db  # use the flask app object created in __init__.py
+from gvlswing_app.urls import Action, URL
 from gvlswing_app.forms import LoginForm, RegistrationForm
 from flask import render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
 from gvlswing_app.models import Administrator
 
 
-@app.route("/")
-@app.route("/index")
+@app.route(URL.root)
+@app.route(URL.index)
 def index():
     return render_template("index.html")
 
 
-@app.route("/admin/control_panel")  # admin landing
+@app.route(URL.admin_control_panel)  # admin landing
 @login_required
 def admin_panel():
     """
@@ -29,12 +30,12 @@ def admin_panel():
     # return "Something went wrong in admin_panel() procedure"
 
 
-@app.route("/admin/login", methods=['GET', 'POST'])  # the login form to grant access
+@app.route(URL.admin_login, methods=['GET', 'POST'])  # the login form to grant access
 def admin_login():
     form = LoginForm()
 
     if current_user.is_authenticated:
-        return redirect(url_for("admin_panel"))
+        return redirect(url_for(Action.admin_panel))
 
     if current_user.is_anonymous and request.method == 'GET':  # allow attempted log in
         return render_template("login.html", form=form)
@@ -44,27 +45,27 @@ def admin_login():
         if user and user.check_password(form.password.data):
             flash("Successful login!")
             login_user(user, remember=form.remember_me.data)
-            return redirect(url_for("admin_panel", username=user.username))
+            return redirect(url_for(Action.admin_panel, username=user.username))
         else:
             flash("Failed login attempt.")
-            return redirect(url_for("admin_panel"))
+            return redirect(url_for(Action.admin_panel))
 
     return "Something went wrong in the admin_login() procedure."
 
 
-@app.route("/logout")
+@app.route(URL.logout)
 def logout():
     logout_user()
     flash("Logged out")
-    return redirect(url_for("index"))
+    return redirect(url_for(Action.index))
 
 
-@app.route("/admin/register", methods=['GET', 'POST'])
+@app.route(URL.admin_register, methods=['GET', 'POST'])
 def admin_register():
     form = RegistrationForm()
     if current_user.is_authenticated:
         flash("You're already logged in")
-        return redirect(url_for("admin_panel"))
+        return redirect(url_for(Action.admin_panel))
     elif request.method == 'GET':
         return render_template("register.html", form=form)
     elif request.method == 'POST' and form.validate_on_submit():
@@ -74,7 +75,7 @@ def admin_register():
         db.session.add(user)
         db.session.commit()
         flash("Registration successful!")
-        return redirect(url_for("index"))
+        return redirect(url_for(Action.index))
 
     return "Something went wrong with admin_register() procedure"
 
--- gvlswing_app/urls.py
+++ gvlswing_app/urls.py
@@ -6,7 +6,22 @@
 
 class Action():
     # these are names of methods called by the routes. Use with url_for(Action.[action_name])
-    index = ''
+    index = 'index'
+    admin_panel = 'admin_panel'
+    admin_login = 'admin_login'
+    admin_register = 'admin_register'
+    logout = 'logout'
+
+
+class URL():
+    root = '/'
+    index = '/index'
+    admin = '/admin'
+    admin_control_panel = admin + '/control_panel'
+    admin_login = admin + '/login'
+    admin_register = admin + '/register'
+    logout = '/logout'
+
 
 
 # end urls
