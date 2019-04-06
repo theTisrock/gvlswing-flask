f63eebf8414f7e59b30027fa01a264bd9abe6205
theTisrock <torok.chris@gmail.com>
2019-04-06 03:29:54 -0400

    "Added a form for content editing."

--- gvlswing_app/forms.py
+++ gvlswing_app/forms.py
@@ -21,16 +21,25 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField("confirm password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField()
 
-    # custom validators used by flask-wtf
-    def validate_username(self, username):
+    def validate_username(self, username):  # flask-wtf validator
         found_match = Administrator.query.filter_by(username=username.data).first()
         if found_match is not None:
             raise ValidationError("Pick a different username.")
 
-    def validate_email(self, email):
+    def validate_email(self, email):  # flask-wtf validator
         found_match = Administrator.query.filter_by(email=email.data).first()
         if found_match is not None:
             raise ValidationError("Pick a different email.")
 
+
+class CMSWelcomeForm(FlaskForm):
+    quantity_dance_duration = StringField("Dance Duration")
+    quantity_lesson_duration = StringField("Lesson(s) Duration")
+    description_price = StringField("Price and/or description")
+    event_frequency = StringField("Frequency. Ex: Every, every other")
+    days_of_week = StringField("Day(s) of the week")
+    timespan_start = StringField("Start Time")
+    timespan_stop = StringField("Stop Time")
+    submit = SubmitField("Save Changes")
 
 # end forms
